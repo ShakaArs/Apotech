4ae2eb17ed14d76197668405af8401e33f36c5ed
Shaka <shaka99rz@gmail.com>
2023-08-22 21:13:26 +0700

    Update perbaikan urutan di transaksi keluar

--- lib/app/modules/transaksikeluar/views/transaksikeluar_view.dart
+++ lib/app/modules/transaksikeluar/views/transaksikeluar_view.dart
@@ -59,133 +59,148 @@ class TransaksiKeluarView extends GetView<TransaksiKeluarController> {
                 ],
               ),
               Obx(
-                () => ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: controller.transaksi.length,
-                  itemBuilder: (context, index) {
-                    var trs = controller.transaksi[index];
-                    int isApproved = trs['is_approved'] ?? 0;
+                () {
+                  List<dynamic> unapprovedTransactions = [];
+                  List<dynamic> approvedTransactions = [];
 
-                    return Container(
-                      margin: EdgeInsets.only(bottom: 20),
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black12,
-                            spreadRadius: 6,
-                            blurRadius: 5,
-                            offset: Offset.fromDirection(1, 6),
-                          ),
-                        ],
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'No Transaksi',
-                                style: GoogleFonts.inter(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                trs['code'] ?? '',
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 39, 178, 83),
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: mediaQueryHeight * 0.04),
-                              Text(
-                                'Nama Penyetor',
-                                style: GoogleFonts.inter(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                trs['name'] ?? '',
-                                style: TextStyle(
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(width: mediaQueryWidth * 0.03),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                isApproved == 0
-                                    ? 'Mengajukan Penarikan'
-                                    : 'Penarikan Disetujui',
-                                style: GoogleFonts.inter(
-                                  fontSize: 11,
-                                  fontWeight: FontWeight.bold,
-                                  color: isApproved == 0
-                                      ? Color.fromARGB(255, 249, 3, 3) // Merah
-                                      : Color.fromARGB(
-                                          255, 3, 168, 18), // Hijau
-                                ),
-                              ),
-                              Text(
-                                trs['amount']?.toString() ?? '',
-                                style: TextStyle(
-                                  color: Color.fromARGB(255, 39, 178, 83),
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              SizedBox(height: mediaQueryHeight * 0.04),
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(100),
-                                  ),
-                                  backgroundColor: isApproved == 0
-                                      ? primary
-                                      : Colors
-                                          .grey, // Primary jika isApproved == 0
-                                  fixedSize: Size(
-                                    mediaQueryWidth * 0.33,
-                                    mediaQueryHeight * 0.05,
-                                  ),
-                                ),
-                                onPressed: isApproved == 0
-                                    ? () {
-                                        controller.approvePenarikan(
-                                          trs['id'] ?? 1,
-                                        );
-                                      }
-                                    : null,
-                                child: Text(
-                                  isApproved == 0
-                                      ? 'Setujui Penarikan'
-                                      : 'Pencairan telah disetujui',
+                  for (var trs in controller.transaksi) {
+                    int isApproved = trs['is_approved'] ?? 0;
+                    if (isApproved == 0) {
+                      unapprovedTransactions.add(trs);
+                    } else {
+                      approvedTransactions.add(trs);
+                    }
+                  }
+
+                  List<dynamic> sortedTransactions = [];
+                  sortedTransactions.addAll(unapprovedTransactions);
+                  sortedTransactions.addAll(approvedTransactions);
+
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: sortedTransactions.length,
+                    itemBuilder: (context, index) {
+                      var trs = sortedTransactions[index];
+                      int isApproved = trs['is_approved'] ?? 0;
+
+                      return Container(
+                        margin: EdgeInsets.only(bottom: 20),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              spreadRadius: 6,
+                              blurRadius: 5,
+                              offset: Offset.fromDirection(1, 6),
+                            ),
+                          ],
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'No Transaksi',
                                   style: GoogleFonts.inter(
-                                    fontSize: 10,
+                                    fontSize: 14,
                                     fontWeight: FontWeight.bold,
                                   ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
+                                Text(
+                                  trs['code'] ?? '',
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 39, 178, 83),
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: mediaQueryHeight * 0.04),
+                                Text(
+                                  'Nama Penyetor',
+                                  style: GoogleFonts.inter(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  trs['name'] ?? '',
+                                  style: TextStyle(
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(width: mediaQueryWidth * 0.03),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  isApproved == 0
+                                      ? 'Mengajukan Penarikan'
+                                      : 'Penarikan Disetujui',
+                                  style: GoogleFonts.inter(
+                                    fontSize: 11,
+                                    fontWeight: FontWeight.bold,
+                                    color: isApproved == 0
+                                        ? Color.fromARGB(255, 249, 3, 3)
+                                        : Color.fromARGB(255, 3, 168, 18),
+                                  ),
+                                ),
+                                Text(
+                                  trs['amount']?.toString() ?? '',
+                                  style: TextStyle(
+                                    color: Color.fromARGB(255, 39, 178, 83),
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: mediaQueryHeight * 0.04),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(100),
+                                    ),
+                                    backgroundColor:
+                                        isApproved == 0 ? primary : Colors.grey,
+                                    fixedSize: Size(
+                                      mediaQueryWidth * 0.33,
+                                      mediaQueryHeight * 0.05,
+                                    ),
+                                  ),
+                                  onPressed: isApproved == 0
+                                      ? () {
+                                          controller.approvePenarikan(
+                                            trs['id'] ?? 1,
+                                          );
+                                        }
+                                      : null,
+                                  child: Text(
+                                    isApproved == 0
+                                        ? 'Setujui Penarikan'
+                                        : 'Pencairan telah disetujui',
+                                    style: GoogleFonts.inter(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
               )
             ],
           ),
